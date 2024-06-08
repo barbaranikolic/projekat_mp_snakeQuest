e83a36be55f9a203b4a38f85a82480e254382434
barbaranikolic <nikolicbarbara22@gmail.com>
2024-06-08 21:36:04 +0200

    basic_changes

---
+++ zmijica/src/dummydata.js
@@ -0,0 +1,103 @@
+export const homeAbout = [
+  {
+    id: 1,
+    title: 'Objective',
+    desc: 'Control the snake using the arrow keys or swipe gestures to eat food and grow longer. Avoid running into the snakes own body.',
+  },
+  {
+    id: 1,
+    title: 'Controls',
+    desc: 'Use the arrow keys (← ↑ → ↓) on your keyboard or swipe gestures on your touchscreen device to change the snakes direction. Press the "Pause" button to pause the game.',
+  },
+  {
+    id: 1,
+    title: 'Scoring',
+    desc: 'Each time the snake eats food, its length increases, and you score points. The game ends when the snake runs into itself.',
+  },
+  {
+    id: 1,
+    title: 'Tips',
+    desc: ' Plan your moves ahead to avoid trapping the snake. Try to create pathways by eating food strategically to prevent getting stuck.',
+  },
+];
+export const homeGallery = [
+  {
+    title:'',
+    image: './images/nostalgia/nostalgija1.jpg',
+  },
+
+  {
+    title:'',
+    image: './images/nostalgia/nostalgija2.webp',
+  },
+  {
+    title:'',
+    image: './images/nostalgia/nostalgija3.jpg',
+  },
+  {
+    title:'',
+    image: './images/nostalgia/nostalgija4.jpg',
+  },
+  {
+    title:'',
+    image: './images/nostalgia/nostalgija5.jpg',
+  },
+  {
+    title:'',
+    image: './images/nostalgia/nostalgija6.jpg',
+  },
+  {
+    title:'',
+    image: './images/nostalgia/nostalgija7.jpg',
+  },
+  {
+    title:'',
+    image: './images/nostalgia/nostalgija8.jpg',
+  },
+];
+export const awrapper = [
+  {
+    cover:'https://img.icons8.com/ios/80/000000/happy.png',
+    data: '3,000',
+    title: 'HAPPY PLAYERS',
+  },
+
+  {
+    cover: 'https://img.icons8.com/ios/80/000000/snake.png',
+    data: 'IRREPLACEBLE',
+    title: 'GAME',
+  },
+  {
+    cover:'https://img.icons8.com/ios/80/000000/money.png',
+    data: 'FOREVER',
+    title: 'FREE',
+  },
+  {
+    cover:'https://img.icons8.com/ios/80/000000/play.png',
+    data: 'PLAY',
+    title: 'NOW',
+  }
+];
+export const team = [
+  {
+    cover: './images/team/Barbara.png',
+    name: 'Barbara Nikolić',
+    work: '2021/1097',
+  },
+  {
+    cover: './images/team/Sara.png',
+    name: 'Sara Memić',
+    work: '2020/011',
+  },
+  {
+    cover: './images/team/Teodora.png',
+    name: 'Teodora Obućina',
+    work: '2020/0491',
+  },
+  {
+    cover: './images/team/Anica.png',
+    name: 'Anica Nedeljković',
+    work: '2020/0030',
+  },
+];
+
