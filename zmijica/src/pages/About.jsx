bdb4fa8a2c80e2d9f54014e2512f59a2b3cb65b2
saramemic8 <memicsara80@gmail.com>
2024-06-08 22:24:47 +0200

    about_page

---
+++ zmijica/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Back from '../components/Back';
+import AboutCard from '../components/About/AboutCard';
+
+const About = () => {
+  return (
+    <>
+      <Back title='About the game' />
+      <AboutCard />
+    </>
+  );
+};
+
+export default About;
