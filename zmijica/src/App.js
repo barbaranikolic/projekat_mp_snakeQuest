f6670e695b2b7feff454cb9531cf10ff0e423fe8
anicanedeljkovic2 <anicanedeljkovic296@gmail.com>
2024-06-08 22:35:59 +0200

    routes

--- zmijica/src/App.js
+++ zmijica/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import GamePage from './pages/GamePage';
+import Team from './pages/Team';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/game' element={<GamePage />} />
+        <Route path='/team' element={<Team />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
